c77600632453236e65e01d605bc7e96a0fd4cfc7
David Frudd <dfrudd@financialforce.com>
2017-09-18 15:33:04 +0100

    Add Lightning Component helper to keep controller lightweight

--- force/main/default/aura/PrimesRequester/PrimesRequesterController.js
+++ force/main/default/aura/PrimesRequester/PrimesRequesterController.js
@@ -1,9 +1,5 @@
 ({
-	requestPrimes : function(component, event, helper) {
-        var action = component.get("c.requestPrimes");
-        action.setParams({ count : component.get("v.count") });
-        action.setCallback(this, function(response) {});
-
-        $A.enqueueAction(action);
-    }
+	onGenerateClick: function (component, event, helper) {
+		helper.requestPrimes(component);
+	}
 })
